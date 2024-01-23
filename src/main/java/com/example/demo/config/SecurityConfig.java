7a260afaa25b6aae773d446b0d3a400bedc40f62
Deepaktuteja1 <115819808+Deepaktuteja1@users.noreply.github.com>
2024-01-24 00:20:12 +0530

    Update SecurityConfig.java

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -39,7 +39,7 @@ public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Excepti
     http.csrf(csrf -> csrf.disable())
             .cors(cors->cors.disable())
             .authorizeHttpRequests(auth->auth.requestMatchers("/home")
-                    .authenticated().requestMatchers("/v3/api-docs/**","swagger-ui/**","/swagger-ui.html","/products/new","/products/authenticate","/products/databaseinfo","/actuator/**","/products/health","/products/send-email")
+                    .authenticated().requestMatchers("/v3/api-docs/**","swagger-ui/**","/swagger-ui.html","/auth/new","/auth/authenticate","/data/fetch","/actuator/**","/actuator/health","/email/send")
                     .permitAll()
                     .anyRequest()
                     .authenticated())

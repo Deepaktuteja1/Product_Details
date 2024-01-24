32f93ce85e1f9e50600c86a22277ef1ed38602fd
Deepaktuteja1 <115819808+Deepaktuteja1@users.noreply.github.com>
2024-01-24 20:48:41 +0530

    Update Schedular.java

--- src/main/java/com/example/demo/utils/Schedular.java
+++ src/main/java/com/example/demo/utils/Schedular.java
@@ -1,24 +1,38 @@
 package com.example.demo.utils;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Schedular {
 
-    // Scheduled email method for demonstration (every 10 minutes)
-    @Scheduled(cron = "0 0/3 * * * *")
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    // Scheduled email method for demonstration (every 30 seconds)
+    @Scheduled(cron = "*/30 * * * * *")
     public void sendScheduledEmail() {
         String to = "fake-email@example.com";
-        String subject = "Scheduled Email: Every 10 minutes";
-        String text = "This is a scheduled email sent every 1 minutes from ProductController.";
-        System.out.println("Every 1 minutes");
-        // You can call EmailAspect's sendEmail method here if needed
+        String subject = "Scheduled Email: Every 30 seconds";
+        String text = "This is a scheduled email sent every 30 seconds from Schedular.";
+        System.out.println("Every 30 seconds");
+
+        sendEmail(to, subject, text);
     }
 
-    // New scheduled method to print a message in the console every 1 minute
-    @Scheduled(cron = "0 0/1 * * * *")
-    public void printConsoleMessage() {
-        System.out.println("Scheduler Working.");
+    private void sendEmail(String to, String subject, String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+        javaMailSender.send(message);
+    }
+
+    @Scheduled(cron = "*/5 * * * * *")
+    public void printScheduledMessage() {
+        System.out.println("Scheduler Message");
     }
 }
